eae1ed0df6b103bc3c88445977087dd8633b5197
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2026-05-30 19:37:32 +0100

    First foundation commit

---
+++ src/main/java/dz/sh/hidra/HidraApplication.java
@@ -0,0 +1,13 @@
+package dz.sh.hidra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dz/sh/hidra/HidraApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.sh.hidra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
